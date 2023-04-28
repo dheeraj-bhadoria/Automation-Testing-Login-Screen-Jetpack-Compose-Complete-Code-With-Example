3d027e19d589fe7630f78d107590b1fbf4bc7bb1
Dheeraj Singh Bhadoria <dheerubhadoria@gmail.com>
2023-04-28 15:36:20 +0530

    Initial release

---
+++ app/src/androidTest/java/com/dheeraj/compose_testcase/LoginScreenTest.kt
@@ -0,0 +1,68 @@
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.dheeraj.compose_testcase.LoginScreen
+import com.dheeraj.compose_testcase.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class LoginScreenTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun login_with_correct_credentials() {
+        // Set up the test
+        composeTestRule.setContent {
+            MaterialTheme {
+                LoginScreen { email, password ->
+                    assert(email == "dsb@gmail.com" && password == "123456")
+                }
+            }
+        }
+
+        // Enter email and password
+        composeTestRule.onNodeWithTag("EmailTextField")
+            .performTextInput("dsb@gmail.com")
+        composeTestRule.onNodeWithTag("PasswordTextField")
+            .performTextInput("123456")
+
+        // Click the login button
+        composeTestRule.onNodeWithTag("LoginButton")
+            .performClick()
+
+        // Verify the success message is displayed
+        composeTestRule.onNodeWithText("Login successful")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun login_with_incorrect_credentials() {
+        // Set up the test
+        composeTestRule.setContent {
+            MaterialTheme {
+                LoginScreen { email, password ->
+                    assert(email != "dsb@gmail.com" || password != "123456")
+                }
+            }
+        }
+
+        // Enter incorrect email and password
+        composeTestRule.onNodeWithTag("EmailTextField")
+            .performTextInput("test@gmail.com")
+        composeTestRule.onNodeWithTag("PasswordTextField")
+            .performTextInput("654321")
+
+        // Click the login button
+        composeTestRule.onNodeWithTag("LoginButton")
+            .performClick()
+
+        // Verify the error message is displayed
+        composeTestRule.onNodeWithText("Wrong credentials")
+            .assertIsDisplayed()
+    }
+}
---
+++ app/src/main/java/com/dheeraj/compose_testcase/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.dheeraj.compose_testcase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import com.dheeraj.compose_testcase.ui.theme.Compose_testcaseTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Compose_testcaseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    LoginScreen { email, password ->
+                        // Here, you can do something with the email and password values, such as validating them or passing them to an authentication function
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginScreen(onLoginClicked: (String, String) -> Unit) {
+    var email by remember { mutableStateOf(TextFieldValue()) }
+    var password by remember { mutableStateOf(TextFieldValue()) }
+    var message by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth().testTag("EmailTextField")
+
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth().testTag("PasswordTextField")
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                if (email.text == "dsb@gmail.com" && password.text == "123456") {
+                    message = "Login successful"
+                    onLoginClicked(email.text, password.text)
+                } else {
+                    message = "Wrong credentials"
+                }
+            },
+            modifier = Modifier.fillMaxWidth().testTag("LoginButton")
+        ) {
+            Text("Login")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        if (message.isNotEmpty()) {
+            Text(text = message)
+        }
+    }
+}
